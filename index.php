3c065745bcb461ed72d6cdbae7bf2df82b7b9866
Owner <Owner@172.16.9.133>
2018-12-07 12:53:48 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: RDP eBook Builder
+Plugin URI: http://robert-d-payne.com/
+Description: Build books from wiki pages. Requires RDP Wiki Embed plugin.
+Version: 0.1.0
+Author: Robert D Payne
+Author URI: http://robert-d-payne.com/
+License: GPLv2 or later
+*/
+
+class RDP_EBB_PLUGIN {
+    
+}//RDP_EBB_PLUGIN
